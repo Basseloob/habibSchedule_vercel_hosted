10aaeaf1775259193d5baccdff4d130300b49adf
Basseloob <Bassel.baragaba@gmail.com>
2023-11-14 17:50:40 +0300

    removed mysql2

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ app.use(express.json());
 app.use(cors());
 
 //Including the Tabel - Schema :
-const db = require("./models");
+// const db = require("./models");
 
 // /\//\/\/\/\//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\//\/\/\/\/\/\/\/\/\/\/\/
 // Routers :
 
-const usersRouter = require("./routes/Users");
-app.use("/auth", usersRouter);
+// const usersRouter = require("./routes/Users");
+// app.use("/auth", usersRouter);
 
 const habibRouter = require("./routes/habibUrl");
 app.use("/habibUrl", habibRouter);
@@ -22,9 +22,9 @@ app.use("/habibUrl", habibRouter);
 // /\//\/\/\/\//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\//\/\/\/\/\/\/\/\/\/\/\/
 // Starting the Server & Connecting to the DB:
 
-db.sequelize.sync().then(() => {
-  const port = 3001;
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+// db.sequelize.sync().then(() => {
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
+// });
