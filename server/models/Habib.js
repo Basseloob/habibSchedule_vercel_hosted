8723542670a36f4a88aebbfac6ed82df6bb665b8
Basseloob <Bassel.baragaba@gmail.com>
2023-11-14 17:57:21 +0300

    removed mysql2

--- server/models/Habib.js
+++ server/models/Habib.js
@@ -1,24 +1,24 @@
-// MySQL Tabel - Schema :
+// // MySQL Tabel - Schema :
 
-module.exports = (sequelize, DataTypes) => {
-  const Habib = sequelize.define("Habib", {
-    HospitalName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    ClinicName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    PhysicianName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    Date: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+// module.exports = (sequelize, DataTypes) => {
+//   const Habib = sequelize.define("Habib", {
+//     HospitalName: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     ClinicName: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     PhysicianName: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     Date: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//   });
 
-  return Habib;
-};
+//   return Habib;
+// };
